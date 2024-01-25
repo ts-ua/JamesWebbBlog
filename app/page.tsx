6d9151260647a705bc27083aa6101ed0c5e50cbd
luffyui <tanaka.yuki000@hotmail.com>
2024-01-24 21:15:32 -0300

    feat: add characteristics page

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import HeroSection from "../components/sections/hero-section";
 import AboutSection from "@/components/sections/about-section";
 import InfoSection from "@/components/sections/info-section";
 import MissionSection from "@/components/sections/mission-section";
+import CharacteristicsSection from "@/components/sections/characteristics-section";
 
 export const revalidate = 15;
 
@@ -14,6 +15,7 @@ export default function Home() {
       <AboutSection />
       <InfoSection />
       <MissionSection />
+      <CharacteristicsSection />
     </main>
   );
 }
---
+++ components/sections/characteristics-section.tsx
@@ -0,0 +1,86 @@
+import { Card, CardBody, CardHeader } from "@nextui-org/react";
+
+const CharacteristicsSection = () => {
+  return (
+    <section className="h-full w-full flex-col bg-neutral-100 px-5 py-10">
+      <div className="mx-auto w-full max-w-6xl">
+        <h3 className="text-3xl font-bold text-center md:text-left">Principais características</h3>
+
+        <div className="mt-5 grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
+          <Card>
+            <CardBody className="p-5">
+              <p className="text-webb mb-2 text-lg font-bold">
+                Maior capacidade de observação
+              </p>
+              <p className="text-sm font-light">
+                O James Webb é capaz de observar objetos celestes mais distantes
+                e com mais detalhes do que os telescópios atuais, devido a sua
+                grande capacidade de coleta de luz e resolução.
+              </p>
+            </CardBody>
+          </Card>
+
+          <Card>
+            <CardBody className="p-5">
+              <p className="text-webb mb-2 text-lg font-bold">Grande espelho</p>
+              <p className="text-sm font-light">
+                Com um espelho de 6,5 metros de diâmetro. O espelho é feito de
+                uma mistura de berílio e ouro, o que lhe permite coletar mais
+                luz e detectar objetos mais fracos e distantes do universo.
+              </p>
+            </CardBody>
+          </Card>
+
+          <Card>
+            <CardBody className="p-5">
+              <p className="text-webb mb-2 text-lg font-bold">Infravermelho</p>
+              <p className="text-sm font-light">
+                O James Webb é especialmente adequado para observar no
+                infravermelho, o que permite observar estrelas e galáxias que
+                não podem ser observadas por telescópios convencionais.
+              </p>
+            </CardBody>
+          </Card>
+
+          <Card>
+            <CardBody className="p-5">
+              <p className="text-webb mb-2 text-lg font-bold">Resfriamento</p>
+              <p className="text-sm font-light">
+                Para melhorar a sensibilidade das observações no infravermelho,
+                o James Webb tem um sistema de resfriamento a nitrogênio
+                líquido, que mantem seguras suas partes sensíveis a temperaturas
+                muito baixas (-233 °C).
+              </p>
+            </CardBody>
+          </Card>
+
+          <Card>
+            <CardBody className="p-5">
+              <p className="text-webb mb-2 text-lg font-bold">Posicionamento</p>
+              <p className="text-sm font-light">
+                O James Webb foi posicionado em um ponto de Lagrange, um ponto
+                gravitacionalmente estável no espaço, a cerca de 1,5 milhões de
+                quilômetros da Terra. Permitindo que o telescópio
+                esteja alinhado com o Sol e a Terra.
+              </p>
+            </CardBody>
+          </Card>
+
+          <Card>
+            <CardBody className="p-5">
+              <p className="text-webb mb-2 text-lg font-bold">Maior precisão</p>
+              <p className="text-sm font-light">
+                O James Webb tem uma precisão muito maior do que o Hubble. Ele
+                pode observar objetos até 100 vezes mais fracos do que o Hubble
+                pode detectar, o que significa que ele pode observar objetos
+                ainda mais distantes e mais fracos.
+              </p>
+            </CardBody>
+          </Card>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default CharacteristicsSection;
