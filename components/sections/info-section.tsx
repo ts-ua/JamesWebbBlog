95f7f1675cbb85b57eccf032a22ae91fe81e3cdc
luffyui <tanaka.yuki000@hotmail.com>
2024-01-25 22:51:48 -0300

    feat: add info text

--- components/sections/info-section.tsx
+++ components/sections/info-section.tsx
@@ -17,8 +17,16 @@ import spectrum from "@/public/spectrum.png";
 
 const InfoSection = () => {
   return (
-    <section className="w-full bg-neutral-100 p-5 lg:px-0">
+    <section className="w-full bg-neutral-100 p-5 lg:px-0 py-10">
       <div className="mx-auto w-full max-w-6xl">
+        <div className="mb-5 flex flex-col items-center justify-center gap-2">
+          <h4 className="text-3xl font-bold">Dados gerais e Instrumentos</h4>
+          <p className="text-sm font-light">
+            Confira abaixo algumas das especificações técnicas e informações
+            gerais do James Webb
+          </p>
+        </div>
+
         <Table isStriped aria-label="Visão geral do James Webb">
           <TableHeader>
             <TableColumn className="text-xl uppercase text-black">
@@ -44,11 +52,15 @@ const InfoSection = () => {
             </TableRow>
             <TableRow key="3">
               <TableCell>FOGUETE DE LANÇAMENTO</TableCell>
-              <TableCell className="flex justify-end text-right">Ariane 5</TableCell>
+              <TableCell className="flex justify-end text-right">
+                Ariane 5
+              </TableCell>
             </TableRow>
             <TableRow key="4">
               <TableCell>MASSA NO LANÇAMENTO</TableCell>
-              <TableCell className="flex justify-end text-right">6,200 kg</TableCell>
+              <TableCell className="flex justify-end text-right">
+                6,200 kg
+              </TableCell>
             </TableRow>
             <TableRow key="5">
               <TableCell>ÓRBITA</TableCell>
@@ -78,7 +90,9 @@ const InfoSection = () => {
             </TableRow>
             <TableRow key="9">
               <TableCell>FAIXA DE COMPRIMENTO DE ONDA</TableCell>
-              <TableCell className="flex justify-end text-right">0,6 μm - 28 μm</TableCell>
+              <TableCell className="flex justify-end text-right">
+                0,6 μm - 28 μm
+              </TableCell>
             </TableRow>
             <TableRow key="10">
               <TableCell>TEMPERATURA DE OPERAÇÃO</TableCell>
@@ -100,13 +114,13 @@ const InfoSection = () => {
             </TableRow>
           </TableBody>
         </Table>
-        <p className="m-2 opacity-50 text-sm">Dados: ESA/Webb</p>
+        <p className="m-2 text-sm opacity-50">Dados: ESA/Webb</p>
         <Divider className="my-8" />
 
         <div className="grid grid-cols-1 gap-4 lg:grid-cols-3">
           <Card>
             <CardHeader className="flex items-center justify-center">
-              <p className="text-webb pt-5 text-xl font-bold">NIRCam</p>
+              <p className="pt-5 text-xl font-bold text-webb">NIRCam</p>
             </CardHeader>
             <CardBody>
               <p className="px-5 pb-5 text-center text-sm font-light">
@@ -121,7 +135,7 @@ const InfoSection = () => {
 
           <Card>
             <CardHeader className="flex items-center justify-center">
-              <p className="text-webb pt-5 text-xl font-bold">MIRI</p>
+              <p className="pt-5 text-xl font-bold text-webb">MIRI</p>
             </CardHeader>
             <CardBody>
               <p className="px-5 pb-5 text-center text-sm font-light">
@@ -136,7 +150,7 @@ const InfoSection = () => {
 
           <Card>
             <CardHeader className="flex items-center justify-center">
-              <p className="text-webb pt-5 text-xl font-bold">NIRSpec</p>
+              <p className="pt-5 text-xl font-bold text-webb">NIRSpec</p>
             </CardHeader>
             <CardBody>
               <p className="px-5 pb-5 text-center text-sm font-light">
@@ -159,7 +173,7 @@ const InfoSection = () => {
           sizes="100vw"
           className="mt-5 h-auto w-full rounded-xl object-cover"
         />
-        <p className="m-2 opacity-50 text-sm">Créditos: ESA/ATG medialab.</p>
+        <p className="m-2 text-sm opacity-50">Créditos: ESA/ATG medialab.</p>
         <p className="px-5 pb-5 text-center text-sm font-light">
           O James Webb é capaz de observar comprimentos de onda de 0,6 a 28,5
           micrômetros, uma faixa muito mais ampla do que a observada pelo
